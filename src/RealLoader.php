f5ef8b82b795d0452deac48d285488a1d72d8b5d
Matias Griese <matias@kunena.org>
2017-12-12 23:09:03 +0200

    Release 5.4.22 - Automatic Version Number Updates

--- src/RealLoader.php
+++ src/RealLoader.php
@@ -45,8 +45,8 @@ abstract class RealLoader
             throw new \LogicException(self::$errorMessageGantryLoaded);
         }
 
-        define('GANTRY5_VERSION', '@version@');
-        define('GANTRY5_VERSION_DATE', '@versiondate@');
+        define('GANTRY5_VERSION', '5.4.22');
+        define('GANTRY5_VERSION_DATE', 'December 12, 2017');
 
         if (!defined('DS')) {
             define('DS', DIRECTORY_SEPARATOR);
